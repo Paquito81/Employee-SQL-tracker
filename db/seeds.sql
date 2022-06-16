22eb56b8b803c3f181b3b8c6c5ecb35ea9ccdb37
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-06-16 00:21:31 -0700

    finish employee table

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUES
+('Jose', 'Saltamonte', null, 4),
+('Jose', 'Rodriguez', null, 3),
+('Fedrick', 'McKenny', 3, 3),
+('David', 'Zabalda', 3, 4),
+('Mateo', 'Andrade', 4, 2),
+('Charles', 'Woolf', 2, 3),
+('Summers', 'Whitenberg', 1, 1),
+('Sarah', 'Anderson', 2, 4),
+('William', 'Chopper', 4, 1);
