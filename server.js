d9932b7a6579510fc9c6a12732c64d1e48fbe219
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-06-17 00:45:39 -0700

    adding routes

--- server.js
+++ server.js
@@ -17,13 +17,90 @@ const db = mysql.createConnection(
     console.log('Conected to the tracker_employee database')
 );
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello homie'
-    });
-});
 app.use((req, res) => {
     res.status(404).end();
+});
+
+db.query(`SELECT * FROM employee`, (err, rows) => {
+    console.log(rows);
+});
+
+app.get('api/employee', (req, res) => {
+    const sql = `SELECT * FROM employee`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+db.query(`SELECT * FROM employee WHERE id = 1`, (err, row)=> {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+app.get('/api/employee/:id', (req, res) => {
+    const sql = `SELECT * FROM employee WHERE id =?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+app.delete('/api/employee/:id', (req, res) => {
+    const sql = `DELETE FROM employee WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({error: res.message});
+
+        }else if (!result.affectedRows) {
+            res.json({
+                message: 'Employee not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+app.post('/api/employee', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'manager_id', 'role_id');
+    if (errors) {
+        res.status(400).json({error: errors});
+        return;
+    }
+});
+
+
+db.query(`DELETE * FROM employee WHERE id =?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+
+    }
+    console.log(result);
+
 });
 
 
