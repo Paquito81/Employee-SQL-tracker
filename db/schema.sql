4ee7d8f45514364f7c7ddcee0e50d7f4f0ecf296
Paquito81 <95262020+Paquito81@users.noreply.github.com>
2022-06-19 04:16:20 -0700

    saving work

--- db/schema.sql
+++ db/schema.sql
@@ -10,8 +10,9 @@ CREATE TABLE department (
 CREATE TABLE role(
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
-    department_id INTEGER NOT NULL
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
 
 );
 
@@ -20,5 +21,7 @@ CREATE TABLE employee(
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
-    manager_id INTEGER
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const db = require('./config/connection');
+const consoleTable = require('console.table');
+const e = require('express');
 
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+// app.use(express.json());
 
 db.connect((err) => {
     if (err) throw err;
@@ -22,14 +24,49 @@ function create() {
                 "View all Employees",
                 "View departments",
                 "View roles",
+                "Update Employee",
                 "Add Employee",
                 "Add Role",
+                "View all Roles",
                 "Add Department",
                 "Update an employee role",
                 'Exit'
-            ]
+            ],
+        },
+    ])
+    .then((answers) => {
+        if (answers.create === "View all Employees") {
+            viewEmployees();
+        
+        } else if (answers.create === "View departments") {
+            viewDepartments();
+
+        } else if (answers.create === "View roles") {
+            viewRoles();
+
+        } else if (answers.create === "Update Employee") {
+
         }
-    ]);
+    });
+};
+
+//view all departments
+function viewDepartments(){
+    var depart = `SELECT * FROM department`;
+      db.query(depart,(err, res) => {
+          if (err) throw err;
+        console.table(res);
+        create();
+      });    
+};
+//view all roles
+function viewRoles(){
+    var rol = `SELECT * FROM role`;
+      db.query(rol,(err, res) => {
+          if (err) throw err;
+        console.table(res);
+        create();
+      });    
 };
 //view all employees
 function viewEmployees(){
@@ -40,20 +77,36 @@ function viewEmployees(){
         create();
       });    
 };
-//view all departments
-function viewDepartments()
-db.query(`SELECT * FROM department`, function (err, data){
-    if (err) throw err;
-    console.table(res);
-    create();
-});
-//view all roles
-function viewRoles()
-db.query(`SELECT * FROM role`, function (err, data){
-    if (err) throw err;
-    console.table(res);
-    create();
-});
+
+function addDepartment() {
+    inquirer
+    .prompt([{
+        name: "name",
+        type:"input",
+        message: "name of the department you are adding."
+    }])
+};
+
+function addRole() {
+    inquirer
+    .prompt([{
+        name: "role",
+        type: "input",
+        message: "What is the name of the role?"
+    },
+    {
+        name: "Salary",
+        type: "input",
+        message:"What will be the salary?"
+    },
+    {
+        name: "department",
+        type: "input",
+        message: "Enter department id."
+    }
+    ])
+};
+
 
 function addEmployee() {
     inquirer
@@ -83,93 +136,89 @@ function addEmployee() {
    ])
 }
 
-function addRole()
-
-function addDepartment()
-
 app.use((req, res) => {
     res.status(404).end();
 });
 
 
 
-app.get('api/employee', (req, res) => {
-    const sql = `SELECT * FROM employee`;
+// app.get('api/employee', (req, res) => {
+//     const sql = `SELECT * FROM employee`;
 
-    db.query(sql, (err, rows) => {
-        if (err) {
-            res.status(500).json({error: err.message});
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: rows
-        });
-    });
-});
+//     db.query(sql, (err, rows) => {
+//         if (err) {
+//             res.status(500).json({error: err.message});
+//             return;
+//         }
+//         res.json({
+//             message: 'success',
+//             data: rows
+//         });
+//     });
+// });
 
-db.query(`SELECT * FROM employee WHERE id = 1`, (err, row)=> {
-    if (err) {
-        console.log(err);
-    }
-    console.log(row);
-});
+// db.query(`SELECT * FROM employee WHERE id = 1`, (err, row)=> {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
 
-app.get('/api/employee/:id', (req, res) => {
-    const sql = `SELECT * FROM employee WHERE id =?`;
-    const params = [req.params.id];
+// app.get('/api/employee/:id', (req, res) => {
+//     const sql = `SELECT * FROM employee WHERE id =?`;
+//     const params = [req.params.id];
 
-    db.query(sql, params, (err, row) => {
-        if (err) {
-            res.status(400).json({error: err.message});
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: row
-        });
-    });
-});
+//     db.query(sql, params, (err, row) => {
+//         if (err) {
+//             res.status(400).json({error: err.message});
+//             return;
+//         }
+//         res.json({
+//             message: 'success',
+//             data: row
+//         });
+//     });
+// });
 
-app.delete('/api/employee/:id', (req, res) => {
-    const sql = `DELETE FROM employee WHERE id = ?`;
-    const params = [req.params.id];
+// app.delete('/api/employee/:id', (req, res) => {
+//     const sql = `DELETE FROM employee WHERE id = ?`;
+//     const params = [req.params.id];
 
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.statusMessage(400).json({error: res.message});
+//     db.query(sql, params, (err, result) => {
+//         if (err) {
+//             res.statusMessage(400).json({error: res.message});
 
-        }else if (!result.affectedRows) {
-            res.json({
-                message: 'Employee not found'
-            });
-        } else {
-            res.json({
-                message: 'deleted',
-                changes: result.affectedRows,
-                id: req.params.id
-            });
-        }
-    });
-});
+//         }else if (!result.affectedRows) {
+//             res.json({
+//                 message: 'Employee not found'
+//             });
+//         } else {
+//             res.json({
+//                 message: 'deleted',
+//                 changes: result.affectedRows,
+//                 id: req.params.id
+//             });
+//         }
+//     });
+// });
 
-app.post('/api/employee', ({ body }, res) => {
-    const errors = inputCheck(body, 'first_name', 'last_name', 'manager_id', 'role_id');
-    if (errors) {
-        res.status(400).json({error: errors});
-        return;
-    }
-});
+// app.post('/api/employee', ({ body }, res) => {
+//     const errors = inputCheck(body, 'first_name', 'last_name', 'manager_id', 'role_id');
+//     if (errors) {
+//         res.status(400).json({error: errors});
+//         return;
+//     }
+// });
 
 
-db.query(`DELETE * FROM employee WHERE id =?`, 1, (err, result) => {
-    if (err) {
-        console.log(err);
+// db.query(`DELETE * FROM employee WHERE id =?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
 
-    }
-    console.log(result);
+//     }
+//     console.log(result);
 
-});
+// });
 
 
 
